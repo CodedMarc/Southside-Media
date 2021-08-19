82bdc8ef4d7acb953d1f61e6d2f595100039e07f
Marc <73245838+CodedMarc@users.noreply.github.com>
2021-08-18 22:59:15 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let bookNow = document.querySelector('.booknow');
+let learnMore = document.querySelector('.learnmore');
+let bookNowTwo = document.querySelector('.booknow2');
+
+bookNow.addEventListener('click', ()=>{
+    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+});
+bookNowTwo.addEventListener('click', ()=>{
+    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+});
+learnMore.addEventListener('click', ()=>{
+    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+});
+
+
